d2c5df7d7f92cba759282219f04991a37fcb4d78
Mattheus <mattheussbarbosa@hotmail.com>
2022-10-26 10:06:17 -0300

    alterei o fundo da parte onde aparecem os produtos da conferência para ficar cinza na quantidade contada estava aparecendo a separação das casas decimais por ponto, alterei para exibir por vírgula para manter o padrão brasileiro

--- lib/procedures/receipt_prodecure/products_conference_page/conference_items.dart
+++ lib/procedures/receipt_prodecure/products_conference_page/conference_items.dart
@@ -68,101 +68,109 @@ class _ConferenceItemsState extends State<ConferenceItems> {
     ConferenceProvider conferenceProvider = Provider.of(context, listen: true);
 
     return Expanded(
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: conferenceProvider.productsCount,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    if (conferenceProvider.isUpdatingQuantity ||
-                        conferenceProvider.consultingProducts) return;
-                    setState(() {
-                      selectedIndex = index;
-                      //isso faz com que apareça os botões de "conferir" e "liberar" somente no item selecionado
-                    });
-                  },
-                  child: PersonalizedCard.personalizedCard(
-                    context: context,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          values(
-                            title: "Nome",
-                            value:
-                                conferenceProvider.products[index].Nome_Produto,
-                            index: index,
-                            conferenceProvider: conferenceProvider,
-                          ),
-                          values(
-                            title: "PLU",
-                            value: conferenceProvider
-                                .products[index].CodigoPlu_ProEmb,
-                            index: index,
-                            conferenceProvider: conferenceProvider,
-                          ),
-                          values(
-                            title: "Embalagem",
-                            value: conferenceProvider
-                                .products[index].PackingQuantity,
-                            index: index,
-                            conferenceProvider: conferenceProvider,
-                          ),
-                          values(
-                            title: "EANs",
-                            value:
-                                conferenceProvider.products[index].AllEans != ""
-                                    ? conferenceProvider.products[index].AllEans
-                                    : "Nenhum",
-                            index: index,
-                            conferenceProvider: conferenceProvider,
-                          ),
-                          if (selectedIndex == index)
-                            //só aparece a quantidade contada se o usuário clicar
-                            //no produto para ele não saber quais produtos já
-                            //foram contados também só aparece a opção de inserir
-                            //a quantidade quando clica no produto
-                            Column(
-                              children: [
-                                values(
-                                  title: "Quantidade contada",
-                                  value: conferenceProvider.products[index]
-                                              .Quantidade_ProcRecebDocProEmb ==
-                                          null
-                                      //se for maior que zero pode colocar no
-                                      //máximo 3 casas decimais. Senão,
-                                      //precisa aparecer o valor que vier na
-                                      //consulta da API
-                                      ? conferenceProvider.products[index]
-                                          .Quantidade_ProcRecebDocProEmb
-                                          .toString()
-                                      : conferenceProvider.products[index]
-                                          .Quantidade_ProcRecebDocProEmb
-                                          .toStringAsFixed(3),
-                                  index: index,
-                                  conferenceProvider: conferenceProvider,
-                                ),
-                                const SizedBox(height: 10),
-                                ConferenceInsertQuantityWidget(
-                                  conferenceProvider: widget.conferenceProvider,
-                                  conferenceProductModel:
-                                      conferenceProvider.products[index],
-                                  docCode: widget.docCode,
-                                  index: index,
-                                ),
-                              ],
+      child: Container(
+        color: Colors.grey,
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: conferenceProvider.productsCount,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      if (conferenceProvider.isUpdatingQuantity ||
+                          conferenceProvider.consultingProducts) return;
+                      setState(() {
+                        selectedIndex = index;
+                        //isso faz com que apareça os botões de "conferir" e "liberar" somente no item selecionado
+                      });
+                    },
+                    child: PersonalizedCard.personalizedCard(
+                      context: context,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            values(
+                              title: "Nome",
+                              value: conferenceProvider
+                                  .products[index].Nome_Produto,
+                              index: index,
+                              conferenceProvider: conferenceProvider,
                             ),
-                        ],
+                            values(
+                              title: "PLU",
+                              value: conferenceProvider
+                                  .products[index].CodigoPlu_ProEmb,
+                              index: index,
+                              conferenceProvider: conferenceProvider,
+                            ),
+                            values(
+                              title: "Embalagem",
+                              value: conferenceProvider
+                                  .products[index].PackingQuantity,
+                              index: index,
+                              conferenceProvider: conferenceProvider,
+                            ),
+                            values(
+                              title: "EANs",
+                              value: conferenceProvider
+                                          .products[index].AllEans !=
+                                      ""
+                                  ? conferenceProvider.products[index].AllEans
+                                  : "Nenhum",
+                              index: index,
+                              conferenceProvider: conferenceProvider,
+                            ),
+                            if (selectedIndex == index)
+                              //só aparece a quantidade contada se o usuário clicar
+                              //no produto para ele não saber quais produtos já
+                              //foram contados também só aparece a opção de inserir
+                              //a quantidade quando clica no produto
+                              Column(
+                                children: [
+                                  values(
+                                    title: "Quantidade contada",
+                                    value: conferenceProvider.products[index]
+                                                .Quantidade_ProcRecebDocProEmb ==
+                                            null
+                                        //se for maior que zero pode colocar no
+                                        //máximo 3 casas decimais. Senão,
+                                        //precisa aparecer o valor que vier na
+                                        //consulta da API
+                                        ? conferenceProvider.products[index]
+                                            .Quantidade_ProcRecebDocProEmb
+                                            .toString()
+                                        : double.tryParse(conferenceProvider
+                                                .products[index]
+                                                .Quantidade_ProcRecebDocProEmb
+                                                .toString())!
+                                            .toStringAsFixed(3)
+                                            .replaceAll(RegExp(r'\.'), ','),
+                                    index: index,
+                                    conferenceProvider: conferenceProvider,
+                                  ),
+                                  const SizedBox(height: 10),
+                                  ConferenceInsertQuantityWidget(
+                                    conferenceProvider:
+                                        widget.conferenceProvider,
+                                    conferenceProductModel:
+                                        conferenceProvider.products[index],
+                                    docCode: widget.docCode,
+                                    index: index,
+                                  ),
+                                ],
+                              ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
