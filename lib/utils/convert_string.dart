81bd3d1af19bdf401a8b0f57fb49f5b4911e4624
Mattheus <mattheussbarbosa@hotmail.com>
2023-04-05 12:59:19 -0300

    Estava ocorrendo erro na consulta de produtos devido a conversão da string errada

--- lib/utils/convert_string.dart
+++ lib/utils/convert_string.dart
@@ -4,22 +4,21 @@ class ConvertString {
   static String convertToBRL(dynamic value) {
     value = value.toString();
 
-    int pointQuantity = ".".allMatches(value).length;
-    for (var x = 0; x < pointQuantity; x++) {
-      if (x != pointQuantity && pointQuantity > 1) {
-        value = value.toString().replaceFirst(RegExp(r'\.'), '');
-      }
-    }
-
     if (value.toString().contains(",")) {
       value = value.toString().replaceAll(RegExp(r','), '.');
     }
 
-    value = double.tryParse(value)!.toStringAsFixed(2);
+    int pointQuantity = ".".allMatches(value).length;
+    for (var x = 1; x < pointQuantity; x++) {
+      if (x < pointQuantity && pointQuantity > 1) {
+        value = value.toString().replaceFirst(RegExp(r'\.'), '');
+      }
+    }
 
-    value = double.parse(value);
+    value = double.tryParse(value.toString())!;
+
     final formatter = new NumberFormat("#,##0.00", "pt_BR");
-    String newText = formatter.format(value) + " R\$";
+    String newText = formatter.format(value) + "R\$";
 
     return newText;
   }
@@ -27,19 +26,19 @@ class ConvertString {
   static String convertToBrazilianNumber(dynamic value) {
     value = value.toString();
 
-    int pointQuantity = ".".allMatches(value).length;
-    for (var x = 0; x < pointQuantity; x++) {
-      if (x < pointQuantity && pointQuantity > 1) {
-        value = value.toString().replaceFirst(RegExp(r'\.'), '');
-      }
-    }
     if (value.toString().contains(",")) {
       value = value.toString().replaceAll(RegExp(r','), '.');
     }
 
-    // value = double.tryParse(value)!.toStringAsFixed(3);
+    int pointQuantity = ".".allMatches(value).length;
+    for (var x = 1; x < pointQuantity; x++) {
+      if (x < pointQuantity && pointQuantity > 1) {
+        value = value.toString().replaceFirst(RegExp(r'\.'), '');
+      }
+    }
 
-    value = double.parse(value);
+    value = double.tryParse(value.toString())!;
+
     final formatter = new NumberFormat("#,##0.000", "pt_BR");
     String newText = formatter.format(value);
 
