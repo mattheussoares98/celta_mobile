8a85ba4c24b83b320dc9823d0eed2d9f7167ea85
Mattheus Soares Barbosa <mattheussbarbosa@hotmail.com>
2024-06-28 16:45:58 -0300

    corrigindo problema para converter dados

--- lib/Models/buy_request/buy_request_buyer_model.dart
+++ lib/Models/buy_request/buy_request_buyer_model.dart
@@ -25,18 +25,6 @@ class BuyRequestBuyerModel {
     required String responseAsString,
     required List listToAdd,
   }) {
-    if (responseAsString.contains("\\")) {
-      //foi corrigido para não ficar mandando "\", "\n" e sinal de " a mais nos retornos. Somente quando estiver desatualido o CMX que vai enviar dessa forma
-      responseAsString = responseAsString
-          .replaceAll(RegExp(r'\\'), '')
-          .replaceAll(RegExp(r'\n'), '')
-          .replaceFirst(RegExp(r'"'), '');
-
-      int lastIndex = responseAsString.lastIndexOf('"');
-      responseAsString =
-          responseAsString.replaceRange(lastIndex, lastIndex + 1, "");
-    }
-
     List responseAsList = json.decode(responseAsString.toString());
     Map responseAsMap = responseAsList.asMap();
 
