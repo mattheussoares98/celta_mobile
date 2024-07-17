ff9059e64fc0089a017b5e4892caebe501fb8e2e
Mattheus <mattheussbarbosa@hotmail.com>
2024-07-17 09:32:17 -0300

    melhoria na visualizacao dos componentes de data

--- lib/Pages/adjust_sale_price/adjust_price/components/initial_and_finish_dates.dart
+++ lib/Pages/adjust_sale_price/adjust_price/components/initial_and_finish_dates.dart
@@ -18,53 +18,62 @@ class InitialAndFinishDates extends StatelessWidget {
     return Column(
       children: [
         const Divider(),
-        Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  const Text(
-                    "Início",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                    ),
-                  ),
-                  Text(initialDate),
-                  TextButton(
-                    child: const Text(
-                      "Alterar data de início",
-                      textAlign: TextAlign.center,
-                    ),
-                    onPressed: updateInitialDate,
-                  ),
-                ],
+        IntrinsicHeight(
+          child: Row(
+            children: [
+              _updateDateWidget(
+                date: initialDate,
+                updateDate: updateInitialDate,
+                isInitialDate: true,
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  const Text(
-                    "Término",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                    ),
-                  ),
-                  Text(finishDate),
-                  TextButton(
-                    child: const Text(
-                      "Alterar data de término",
-                      textAlign: TextAlign.center,
-                    ),
-                    onPressed: updateFinishDate,
-                  ),
-                ],
+              const VerticalDivider(
+                color: Colors.black,
+                thickness: 0.2,
               ),
-            ),
-          ],
+              _updateDateWidget(
+                date: finishDate,
+                updateDate: updateFinishDate,
+                isInitialDate: false,
+              ),
+            ],
+          ),
         ),
       ],
     );
   }
+}
+
+Widget _updateDateWidget({
+  required String date,
+  required Function() updateDate,
+  required bool isInitialDate,
+}) {
+  return Expanded(
+    child: Column(
+      children: [
+        Text(
+          isInitialDate ? "Data de início" : "Data de término",
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+        ),
+        Text(date),
+        TextButton(
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Alterar data",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(width: 10),
+              Icon(Icons.date_range)
+            ],
+          ),
+          onPressed: updateDate,
+        ),
+      ],
+    ),
+  );
 }
