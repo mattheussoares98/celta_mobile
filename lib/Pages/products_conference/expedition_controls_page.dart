7d9aab57bb96cb21d925a88981cc132fbaca6b07
Mattheus <mattheussbarbosa@hotmail.com>
2024-08-07 14:59:02 -0300

    consultando processos de expedicao

--- lib/Pages/products_conference/expedition_controls_page.dart
+++ lib/Pages/products_conference/expedition_controls_page.dart
@@ -1,15 +1,69 @@
+import 'package:celta_inventario/components/components.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ExpeditionControlsPage extends StatelessWidget {
+import '../../models/enterprise/enterprise.dart';
+import '../../providers/providers.dart';
+
+class ExpeditionControlsPage extends StatefulWidget {
   const ExpeditionControlsPage({super.key});
 
   @override
+  State<ExpeditionControlsPage> createState() => _ExpeditionControlsPageState();
+}
+
+class _ExpeditionControlsPageState extends State<ExpeditionControlsPage> {
+  Future<void> _getExpeditionControlsToConference() async {
+    await Future.delayed(Duration.zero);
+
+    ProductsConferenceProvider productsConferenceProvider =
+        Provider.of(context, listen: false);
+
+    EnterpriseModel enterprise =
+        ModalRoute.of(context)!.settings.arguments as EnterpriseModel;
+
+    await productsConferenceProvider.getExpeditionControlsToConference(
+      enterpriseCode: enterprise.codigoInternoEmpresa,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getExpeditionControlsToConference();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Controles de expedição"),
-      ),
-      body: const Center(child: Text("Controles de expedição")),
+    ProductsConferenceProvider productsConferenceProvider =
+        Provider.of(context);
+
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: const Text("Controles de expedição"),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Controles de expedição"),
+                if (productsConferenceProvider.errorMessage != "")
+                  ErrorMessage(
+                    errorMessage: productsConferenceProvider.errorMessage,
+                  ),
+              ],
+            ),
+          ),
+        ),
+        loadingWidget(
+          message: "Aguarde...",
+          isLoading: productsConferenceProvider.isLoading,
+        )
+      ],
     );
   }
 }
--- lib/providers/products_conference_provider.dart
+++ lib/providers/products_conference_provider.dart
@@ -1,3 +1,47 @@
+import 'dart:convert';
+
+import 'package:celta_inventario/api/api.dart';
+import 'package:celta_inventario/utils/utils.dart';
 import 'package:flutter/material.dart';
 
-class ProductsConferenceProvider with ChangeNotifier {}
+class ProductsConferenceProvider with ChangeNotifier {
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  String _errorMessage = "";
+  String get errorMessage => _errorMessage;
+
+  Future<void> getExpeditionControlsToConference({
+    required int enterpriseCode,
+  }) async {
+    _errorMessage = "";
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final jsonFilters = {
+        "CrossIdentity": UserData.crossIdentity,
+        "EnterpriseCode": enterpriseCode,
+      };
+
+      await SoapRequest.soapPost(
+        parameters: {
+          "stringFilters": json.encode(jsonFilters),
+        },
+        typeOfResponse: "GetExpeditionControlsToConferenceResponse",
+        SOAPAction: "GetExpeditionControlsToConference",
+        serviceASMX: "celtaproductservice.asmx",
+        typeOfResult: "GetExpeditionControlsToConferenceResult",
+      );
+
+      SoapRequestResponse.responseAsMap;
+      SoapRequestResponse.responseAsString;
+      _errorMessage = SoapRequestResponse.errorMessage;
+    } catch (e) {
+      _errorMessage = DefaultErrorMessageToFindServer.ERROR_MESSAGE;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
