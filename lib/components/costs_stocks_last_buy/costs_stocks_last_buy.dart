3dc03ad30be57412f3d29a21fd1f93b77158a161
Mattheus <mattheussbarbosa@hotmail.com>
2024-08-08 16:33:39 -0300

    movendo o componente de ultima entrada do produto

--- lib/components/costs_stocks_last_buy/costs_stocks_last_buy.dart
+++ lib/components/costs_stocks_last_buy/costs_stocks_last_buy.dart
@@ -1,2 +1,1 @@
 export 'costs_stocks_last_buy_widget.dart';
-export 'last_buy_entrance.dart';
--- lib/components/product_informations/product_informations.dart
+++ lib/components/product_informations/product_informations.dart
@@ -1,5 +1,6 @@
 export './action_button.dart';
 export './associated_stocks.dart';
 export './costs.dart';
+export './last_buy_entrance.dart';
 export 'open_dialog_product_informations.dart';
 export './stocks.dart';
