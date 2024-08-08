2ba738c8618d2bffe7b1149c5cf81b28daf21450
Mattheus <mattheussbarbosa@hotmail.com>
2024-08-08 16:29:43 -0300

    movendo o local do componente de custos

--- lib/components/costs_stocks_last_buy/costs_stocks_last_buy.dart
+++ lib/components/costs_stocks_last_buy/costs_stocks_last_buy.dart
@@ -1,3 +1,2 @@
 export 'costs_stocks_last_buy_widget.dart';
-export 'costs.dart';
 export 'last_buy_entrance.dart';
--- lib/components/product_informations/product_informations.dart
+++ lib/components/product_informations/product_informations.dart
@@ -1,4 +1,5 @@
 export './action_button.dart';
 export './associated_stocks.dart';
+export './costs.dart';
 export './show_all_stocks.dart';
 export './stocks.dart';
