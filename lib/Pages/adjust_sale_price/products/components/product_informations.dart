fd76cc1dd890b47f30fc16147b81d1797e18d900
Mattheus <mattheussbarbosa@hotmail.com>
2024-09-23 11:48:23 -0300

    going to adjustSalePrice when click in the product

--- lib/Pages/adjust_sale_price/products/components/product_informations.dart
+++ lib/Pages/adjust_sale_price/products/components/product_informations.dart
@@ -14,85 +14,94 @@ class ProductInformations extends StatelessWidget {
     super.key,
   });
 
+  void onTapProduct(BuildContext context, EnterpriseModel enterprise) {
+    Navigator.of(context).pushNamed(
+      APPROUTES.ADJUST_SALE_PRICE,
+      arguments: {
+        "enterprise": enterprise,
+        "product": product,
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final EnterpriseModel enterprise =
         ModalRoute.of(context)!.settings.arguments! as EnterpriseModel;
 
-    return Column(
-      children: [
-        TitleAndSubtitle.titleAndSubtitle(
-          // title: "Produto",
-          subtitle: "${product.name} (${product.packingQuantity})",
-        ),
-        TitleAndSubtitle.titleAndSubtitle(
-          title: "PLU",
-          subtitle: product.plu,
-        ),
-        TitleAndSubtitle.titleAndSubtitle(
-          subtitle: product.retailPracticedPrice != null &&
-                  product.retailPracticedPrice! > 0
-              ? "Varejo: " +
-                  product.retailPracticedPrice
-                      .toString()
-                      .toBrazilianNumber()
-                      .addBrazilianCoin()
-              : "Sem preço de varejo",
-          subtitleColor: product.retailPracticedPrice != null &&
-                  product.retailPracticedPrice! > 0
-              ? Theme.of(context).colorScheme.primary
-              : Colors.black,
-        ),
-        TitleAndSubtitle.titleAndSubtitle(
-          subtitle: product.wholePracticedPrice != null &&
-                  product.wholePracticedPrice! > 0
-              ? "Atacado: " +
-                  product.wholePracticedPrice
-                      .toString()
-                      .toBrazilianNumber()
-                      .addBrazilianCoin()
-              : "Sem preço de atacado",
-          subtitleColor: product.wholePracticedPrice != null &&
-                  product.wholePracticedPrice! > 0
-              ? Theme.of(context).colorScheme.primary
-              : Colors.black,
-        ),
-        TitleAndSubtitle.titleAndSubtitle(
-          subtitle: product.eCommercePracticedPrice != null &&
-                  product.eCommercePracticedPrice! > 0
-              ? "Ecommerce: " +
-                  product.eCommercePracticedPrice
-                      .toString()
-                      .toBrazilianNumber()
-                      .addBrazilianCoin()
-              : "Sem preço de ecommerce",
-          subtitleColor: product.eCommercePracticedPrice != null &&
-                  product.eCommercePracticedPrice! > 0
-              ? Theme.of(context).colorScheme.primary
-              : Colors.black,
-          otherWidget: InkWell(
-            onTap: () {
-              Navigator.of(context).pushNamed(
-                APPROUTES.ADJUST_SALE_PRICE,
-                arguments: {
-                  "enterprise": enterprise,
-                  "product": product,
-                },
-              );
-            },
-            child: Row(
-              children: [
-                Text(
-                  "Alterar preços",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
+    return InkWell(
+      onTap: () {
+        onTapProduct(context, enterprise);
+      },
+      child: Column(
+        children: [
+          TitleAndSubtitle.titleAndSubtitle(
+            // title: "Produto",
+            subtitle: "${product.name} (${product.packingQuantity})",
+          ),
+          TitleAndSubtitle.titleAndSubtitle(
+            title: "PLU",
+            subtitle: product.plu,
+          ),
+          TitleAndSubtitle.titleAndSubtitle(
+            subtitle: product.retailPracticedPrice != null &&
+                    product.retailPracticedPrice! > 0
+                ? "Varejo: " +
+                    product.retailPracticedPrice
+                        .toString()
+                        .toBrazilianNumber()
+                        .addBrazilianCoin()
+                : "Sem preço de varejo",
+            subtitleColor: product.retailPracticedPrice != null &&
+                    product.retailPracticedPrice! > 0
+                ? Theme.of(context).colorScheme.primary
+                : Colors.black,
+          ),
+          TitleAndSubtitle.titleAndSubtitle(
+            subtitle: product.wholePracticedPrice != null &&
+                    product.wholePracticedPrice! > 0
+                ? "Atacado: " +
+                    product.wholePracticedPrice
+                        .toString()
+                        .toBrazilianNumber()
+                        .addBrazilianCoin()
+                : "Sem preço de atacado",
+            subtitleColor: product.wholePracticedPrice != null &&
+                    product.wholePracticedPrice! > 0
+                ? Theme.of(context).colorScheme.primary
+                : Colors.black,
+          ),
+          TitleAndSubtitle.titleAndSubtitle(
+            subtitle: product.eCommercePracticedPrice != null &&
+                    product.eCommercePracticedPrice! > 0
+                ? "Ecommerce: " +
+                    product.eCommercePracticedPrice
+                        .toString()
+                        .toBrazilianNumber()
+                        .addBrazilianCoin()
+                : "Sem preço de ecommerce",
+            subtitleColor: product.eCommercePracticedPrice != null &&
+                    product.eCommercePracticedPrice! > 0
+                ? Theme.of(context).colorScheme.primary
+                : Colors.black,
+            otherWidget: InkWell(
+              onTap: () {
+                onTapProduct(context, enterprise);
+              },
+              child: Row(
+                children: [
+                  Text(
+                    "Alterar preços",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
