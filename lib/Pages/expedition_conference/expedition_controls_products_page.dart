35334d9720998b397c12a8bc6934825d389eef82
Mattheus <mattheussbarbosa@hotmail.com>
2024-09-24 15:29:52 -0300

    adding options in ExpeditionControlsProductsPage

--- lib/Pages/expedition_conference/expedition_controls_products_page.dart
+++ lib/Pages/expedition_conference/expedition_controls_products_page.dart
@@ -1,19 +1,43 @@
+import 'package:celta_inventario/components/components.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/providers.dart';
 
 class ExpeditionControlsProductsPage extends StatelessWidget {
   const ExpeditionControlsProductsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ExpeditionConferenceProvider expeditionConferenceProvider =
+        Provider.of(context);
+
     return PopScope(
-      onPopInvokedWithResult: (_, __) {},
-      child: Scaffold(
-        appBar: AppBar(
-          title: const FittedBox(child: Text("Conferência de produtos")),
-        ),
-        body: const Center(
-          child: Text("Conferência de produtos"),
-        ),
+      onPopInvokedWithResult: (_, __) {
+        //TODO
+      },
+      child: Stack(
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              title: const FittedBox(child: Text("Conferência de produtos")),
+              actions: [
+                IconButton(
+                    onPressed: () async {
+                      //TODO
+                    },
+                    icon: const Icon(Icons.refresh))
+              ],
+            ),
+            body: const Center(
+              child: Text("Conferência de produtos"),
+            ),
+          ),
+          loadingWidget(
+            message: "Aguarde...",
+            isLoading: expeditionConferenceProvider.isLoading,
+          ),
+        ],
       ),
     );
   }
