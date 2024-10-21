8fb4e78beb9cfdda6e46ef4ba049e75da6e67825
Mattheus <mattheussbarbosa@hotmail.com>
2024-10-21 14:57:55 -0300

    adding enterprise property in stocks model

--- lib/Models/soap/products/get_product_json/stocks_model.dart
+++ lib/Models/soap/products/get_product_json/stocks_model.dart
@@ -1,18 +1,25 @@
 class StocksModel {
   String? stockName;
   double? stockQuantity;
+  String? enterprise;
 
-  StocksModel({this.stockName, this.stockQuantity});
+  StocksModel({
+    required this.stockName,
+    required this.stockQuantity,
+    required this.enterprise,
+  });
 
   StocksModel.fromJson(Map<String, dynamic> json) {
     stockName = json['StockName'];
     stockQuantity = json['StockQuantity'];
+    enterprise = json['Enterprise'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['StockName'] = this.stockName;
     data['StockQuantity'] = this.stockQuantity;
+    data['Enterprise'] = this.enterprise;
     return data;
   }
 }
