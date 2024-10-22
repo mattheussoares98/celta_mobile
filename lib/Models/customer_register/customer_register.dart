edc9a210d9a1f3106e6fefb662b67e386fc070c7
Mattheus <mattheussbarbosa@hotmail.com>
2024-10-22 14:04:25 -0300

    creating CustomerRegisterBindedCovenantModel

--- lib/Models/customer_register/customer_register.dart
+++ lib/Models/customer_register/customer_register.dart
@@ -1,3 +1,4 @@
+export './customer_register_binded_covenant_model.dart';
 export 'customer_register_cep.dart';
 export './customer_register_covenant_model.dart';
 export 'customer_register_model.dart';
---
+++ lib/Models/customer_register/customer_register_binded_covenant_model.dart
@@ -0,0 +1,22 @@
+import 'customer_register.dart';
+
+class CustomerRegisterBindedCovenantModel {
+  final CustomerRegisterCovenantModel customerRegisterCovenantModel;
+  final double limit;
+
+  const CustomerRegisterBindedCovenantModel({
+    required this.customerRegisterCovenantModel,
+    required this.limit,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "customerRegisterCovenantModel": customerRegisterCovenantModel,
+        "limit": limit,
+      };
+
+  factory CustomerRegisterBindedCovenantModel.fromJson(Map data) =>
+      CustomerRegisterBindedCovenantModel(
+        customerRegisterCovenantModel: data["customerRegisterCovenantModel"],
+        limit: data["limit"],
+      );
+}
