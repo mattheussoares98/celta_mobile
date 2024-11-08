c5254c6d611a51b2ba5ab4b8f6d46ccbc5a00ad6
Mattheus <mattheussbarbosa@hotmail.com>
2024-11-08 09:46:34 -0300

    adding new property in StocksModel

--- lib/Models/soap/products/get_product_json/stocks_model.dart
+++ lib/Models/soap/products/get_product_json/stocks_model.dart
@@ -2,17 +2,20 @@ class StocksModel {
   String? stockName;
   double? stockQuantity;
   String? enterprise;
+  int? enterpriseCode;
 
   StocksModel({
     required this.stockName,
     required this.stockQuantity,
     required this.enterprise,
+    required this.enterpriseCode,
   });
 
   StocksModel.fromJson(Map<String, dynamic> json) {
     stockName = json['StockName'];
     stockQuantity = json['StockQuantity'];
     enterprise = json['Enterprise'];
+    enterpriseCode = json['EnterpriseCode'];
   }
 
   Map<String, dynamic> toJson() {
@@ -20,6 +23,7 @@ class StocksModel {
     data['StockName'] = this.stockName;
     data['StockQuantity'] = this.stockQuantity;
     data['Enterprise'] = this.enterprise;
+    data['EnterpriseCode'] = this.enterpriseCode;
     return data;
   }
 }
