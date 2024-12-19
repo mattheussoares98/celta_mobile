b387e086716115b26e2ebbca38ae95530a38d82a
Mattheus <mattheussbarbosa@hotmail.com>
2024-12-19 13:59:26 -0300

    creating separeted BuyersDropdown widget

---
+++ lib/components/buyers/buyers.dart
@@ -0,0 +1,1 @@
+export 'buyers_dropdown.dart';
---
+++ lib/components/buyers/buyers_dropdown.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import '../../models/soap/soap.dart';
+
+class BuyersDropDown extends StatelessWidget {
+  final GlobalKey<FormFieldState> dropdownKey;
+  final String disabledHintText;
+  final List<BuyerModel> buyers;
+  final BuyerModel? value;
+  final String? Function(BuyerModel?)? validator;
+  final void Function(BuyerModel?)? onChanged;
+  final void Function()? reloadBuyers;
+  final void Function() onTap;
+  final bool showRefreshIcon;
+
+  const BuyersDropDown({
+    required this.value,
+    required this.dropdownKey,
+    required this.disabledHintText,
+    required this.buyers,
+    required this.onChanged,
+    required this.reloadBuyers,
+    required this.showRefreshIcon,
+    required this.onTap,
+    this.validator,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Card(
+            child: DropdownButtonFormField<BuyerModel>(
+              key: dropdownKey,
+              value: value,
+              disabledHint: Center(child: Text(disabledHintText)),
+              isExpanded: true,
+              hint: Center(
+                child: Text(
+                  disabledHintText,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              validator: validator,
+              onChanged: onChanged,
+              items: buyers
+                  .map(
+                    (value) => DropdownMenuItem(
+                      value: value,
+                      alignment: Alignment.center,
+                      onTap: onTap,
+                      child: FittedBox(
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Text(
+                                value.Name,
+                              ),
+                            ),
+                            const Divider(
+                              height: 4,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+        if (showRefreshIcon)
+          IconButton(
+            onPressed: reloadBuyers,
+            tooltip: "Pesquisar compradores novamente",
+            icon: Icon(
+              Icons.refresh,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+      ],
+    );
+  }
+}
