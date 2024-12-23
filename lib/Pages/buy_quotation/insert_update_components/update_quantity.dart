1f636d840871f1c3c4f70dd856b7c37571f21de2
Mattheus <mattheussbarbosa@hotmail.com>
2024-12-23 16:54:52 -0300

    validating number

--- lib/Pages/buy_quotation/insert_update_components/update_quantity.dart
+++ lib/Pages/buy_quotation/insert_update_components/update_quantity.dart
@@ -17,64 +17,85 @@ class UpdateQuantity extends StatelessWidget {
     super.key,
   });
 
+  static final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     BuyQuotationProvider buyQuotationProvider = Provider.of(context);
-    return Column(
-      children: [
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: buyQuotationProvider.selectedEnterprises.length,
-          itemBuilder: (context, index) {
-            final enterprise = buyQuotationProvider.selectedEnterprises[index];
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: buyQuotationProvider.selectedEnterprises.length,
+            itemBuilder: (context, index) {
+              final enterprise =
+                  buyQuotationProvider.selectedEnterprises[index];
 
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: TextFormField(
-                controller: controllers[index],
-                style: const TextStyle(fontSize: 14),
-                decoration: FormFieldDecoration.decoration(
-                  context: context,
-                  labelText: "Qtd ${enterprise.Name}",
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: TextFormField(
+                  controller: controllers[index],
+                  style: const TextStyle(fontSize: 14),
+                  validator: (value) {
+                    if (value == null || value.isEmpty == true) {
+                      return null;
+                    } else if (value.toDouble() == -1) {
+                      return "Número inválido";
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: FormFieldDecoration.decoration(
+                    context: context,
+                    labelText: "Qtd ${enterprise.Name}",
+                  ),
                 ),
-              ),
-            );
-          },
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: TextButton(
-                onPressed: updateSelectedIndex,
-                child: const Text(
-                  "Cancelar",
-                  style: TextStyle(
-                    color: Colors.red,
+              );
+            },
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: updateSelectedIndex,
+                  child: const Text(
+                    "Cancelar",
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: TextButton(
-                onPressed: () {
-                  buyQuotationProvider.updateProductQuantity(
-                    quantitys:
-                        controllers.map((e) => e.text.toDouble()).toList(),
-                    productIndex: productIndex,
-                  );
-                  updateSelectedIndex();
-                },
-                child: Text(
-                  "Alterar",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    bool? isValid = _formKey.currentState?.validate();
+
+                    if (isValid != true) {
+                      return;
+                    }
+
+                    buyQuotationProvider.updateProductQuantity(
+                      quantitys:
+                          controllers.map((e) => e.text.toDouble()).toList(),
+                      productIndex: productIndex,
+                    );
+                    updateSelectedIndex();
+                  },
+                  child: Text(
+                    "Alterar",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
