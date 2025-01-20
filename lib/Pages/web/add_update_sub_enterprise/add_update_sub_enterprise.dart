3cbf22c82f4ec8e3dbfbe95bfed817d14af59a66
Mattheus <mattheussbarbosa@hotmail.com>
2025-01-20 14:14:08 -0300

    creating page addUpdateEnterprise

---
+++ lib/Pages/web/add_update_sub_enterprise/add_update_sub_enterprise.dart
@@ -0,0 +1,2 @@
+export './components/components.dart';
+export './add_update_sub_enterprise_page.dart';
---
+++ lib/Pages/web/add_update_sub_enterprise/add_update_sub_enterprise_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddUpdateSubEnterprisePage extends StatelessWidget {
+  const AddUpdateSubEnterprisePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Update Sub Enterprise'),
+      ),
+      body: Center(
+        child: Text('Add Update Sub Enterprise Page'),
+      ),
+    );
+  }
+}
