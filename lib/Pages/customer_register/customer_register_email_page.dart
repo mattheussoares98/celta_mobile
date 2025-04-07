a56a1dbb49e1303d394c589456993ac17838d7f0
Mattheus <mattheussbarbosa@hotmail.com>
2025-04-07 16:49:33 -0300

    changing keyboard type to be like email

--- lib/Pages/customer_register/customer_register_email_page.dart
+++ lib/Pages/customer_register/customer_register_email_page.dart
@@ -55,6 +55,7 @@ class _CustomerRegisterEmailPageState extends State<CustomerRegisterEmailPage> {
           children: [
             FormFieldWidget(
               enabled: true,
+              keyboardType: TextInputType.emailAddress,
               focusNode: emailFocusNode,
               onFieldSubmitted: (String? value) {
                 addEmail(customerRegisterProvider: customerRegisterProvider);
