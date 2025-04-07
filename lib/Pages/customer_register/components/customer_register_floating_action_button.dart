2ad64ce7f45af13541f508d788e691e6dd74f453
Mattheus <mattheussbarbosa@hotmail.com>
2025-04-07 11:51:30 -0300

    getting correct text

--- lib/Pages/customer_register/components/customer_register_floating_action_button.dart
+++ lib/Pages/customer_register/components/customer_register_floating_action_button.dart
@@ -21,119 +21,104 @@ class CustomerRegisterFloatingActionButton extends StatefulWidget {
 
 class _CustomerRegisterFloatingActionButtonState
     extends State<CustomerRegisterFloatingActionButton> {
+  String buttonText(CustomerRegisterProvider customerRegisterProvider) {
+    final name = customerRegisterProvider.customer?.Name;
+    final cpfCnpj = customerRegisterProvider.customer?.CpfCnpjNumber;
+    if (name == null || name.isEmpty || cpfCnpj == null || cpfCnpj.isEmpty) {
+      return "Adicione\ndados\npessoais";
+    } else if ((customerRegisterProvider.customer?.Addresses?.length ?? 0) ==
+        0) {
+      return "Adicione\num\nendereço";
+    } else {
+      return "SALVAR";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     CustomerRegisterProvider customerRegisterProvider = Provider.of(context);
     AddressProvider addressProvider = Provider.of(context);
 
     return GestureDetector(
-      onTap: customerRegisterProvider.isLoading
-          ? null
-          : () async {
-              if (customerRegisterProvider.customer?.Addresses?.length == 0) {
+      onTap: () async {
+        final customer = customerRegisterProvider.customer;
+        if (customer?.Name == null ||
+            customer?.CpfCnpjNumber?.isEmpty == true) {
+          setState(() {
+            widget.updateSelectedIndex(0);
+          });
+        } else if ((customerRegisterProvider.customer?.Addresses?.length ??
+                0) ==
+            0) {
+          setState(() {
+            widget.updateSelectedIndex(1);
+          });
+        } else {
+          ShowAlertDialog.show(
+            context: context,
+            title: "Cadastrar cliente",
+            content: const SingleChildScrollView(
+              child: Text(
+                "Deseja confirmar o cadastro do cliente?",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            function: () async {
+              await customerRegisterProvider.insertCustomer(
+                addressProvider: addressProvider,
+                password: widget.passwordController.text,
+              );
+
+              if (customerRegisterProvider.errorMessage == "") {
                 setState(() {
-                  widget.updateSelectedIndex(1);
+                  widget.updateSelectedIndex(0);
                 });
-              } else {
-                ShowAlertDialog.show(
+
+                ShowSnackbarMessage.show(
+                  message: "Cliente inserido/atualizado com sucesso",
+                  backgroundColor: Theme.of(context).colorScheme.primary,
                   context: context,
-                  title: "Cadastrar cliente",
-                  content: const SingleChildScrollView(
-                    child: Text(
-                      "Deseja confirmar o cadastro do cliente?",
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  function: () async {
-                    await customerRegisterProvider.insertCustomer(
-                      addressProvider: addressProvider,
-                      password: widget.passwordController.text,
-                    );
-
-                    if (customerRegisterProvider.errorMessage == "") {
-                      setState(() {
-                        widget.updateSelectedIndex(0);
-                      });
-
-                      ShowSnackbarMessage.show(
-                        message: "Cliente inserido/atualizado com sucesso",
-                        backgroundColor: Theme.of(context).colorScheme.primary,
-                        context: context,
-                      );
-                    } else {
-                      ShowSnackbarMessage.show(
-                        message: customerRegisterProvider.errorMessage,
-                        context: context,
-                      );
-                    }
-                  },
+                );
+              } else {
+                ShowSnackbarMessage.show(
+                  message: customerRegisterProvider.errorMessage,
+                  context: context,
                 );
               }
             },
+          );
+        }
+      },
       child: CircleAvatar(
         minRadius: 35,
         maxRadius: 35,
-        backgroundColor: customerRegisterProvider.isLoading
-            ? Colors.grey[300]
-            : Theme.of(context).colorScheme.primary,
-        child: customerRegisterProvider.isLoading
-            ? FittedBox(
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        "SALVANDO",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: Container(
-                          height: 8,
-                          width: 100,
-                          child: LinearProgressIndicator(
-                            minHeight: 2,
-                            backgroundColor: Colors.grey[300],
-                          ),
-                        ),
-                      ),
-                    ],
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FittedBox(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  buttonText(customerRegisterProvider),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              )
-            : Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FittedBox(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        (customerRegisterProvider.customer?.Addresses?.length ??
-                                    0) >
-                                0
-                            ? "SALVAR"
-                            : "Adicione\num\nendereço",
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      if ((customerRegisterProvider
-                                  .customer?.Addresses?.length ??
-                              0) >
-                          0)
-                        const Icon(
-                          Icons.person_add,
-                          color: Colors.white,
-                          size: 50,
-                        ),
-                    ],
+                if ((customerRegisterProvider.customer?.Addresses?.length ??
+                        0) >
+                    0)
+                  const Icon(
+                    Icons.person_add,
+                    color: Colors.white,
+                    size: 50,
                   ),
-                ),
-              ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
