fedbb0d9874a920993deded2d4cd4078a5092a5c
Mattheus <mattheussbarbosa@hotmail.com>
2025-05-15 10:07:12 -0300

    adding new property of fiscalCode

--- lib/Models/products/get_product_json/last_buy_entrance_model.dart
+++ lib/Models/products/get_product_json/last_buy_entrance_model.dart
@@ -3,12 +3,14 @@ class LastBuyEntranceModel {
   String? entranceDate;
   String? supplier;
   double? quantity;
+  String? fiscalCode;
 
   LastBuyEntranceModel({
     required this.number,
     required this.entranceDate,
     required this.supplier,
     required this.quantity,
+    required this.fiscalCode,
   });
 
   LastBuyEntranceModel.fromJson(Map<String, dynamic> json) {
@@ -16,6 +18,7 @@ class LastBuyEntranceModel {
     entranceDate = json['EntranceDate'];
     supplier = json['Supplier'];
     quantity = json['Quantity'];
+    fiscalCode = json['FiscalCode'];
   }
 
   Map<String, dynamic> toJson() {
@@ -24,6 +27,7 @@ class LastBuyEntranceModel {
     data['EntranceDate'] = this.entranceDate;
     data['Supplier'] = this.supplier;
     data['Quantity'] = this.quantity;
+    data['FiscalCode'] = this.fiscalCode;
     return data;
   }
 }
